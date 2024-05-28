f4fb0673710f07126ea4eb0ad1d87c6078b0ee26
SSong-sh <tjdgp0817@gmail.com>
2024-05-28 12:47:02 +0900

    Spring boot setting + lombok + jsp

---
+++ boot-begin/src/main/java/com/test/bootbegin/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.test.bootbegin.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test.do")
+    public String test() {
+
+        return "스프링 부트입니다.";
+    }
+
+    @GetMapping("/hello.do")
+    public String hello() {
+
+        return "안녕. 스프링 부트입니다.";
+    }
+
+}
