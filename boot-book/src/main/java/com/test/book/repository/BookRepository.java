1ec9318d0d6069137e21ca02f9020cb945989127
SSong-sh <tjdgp0817@gmail.com>
2024-06-14 14:21:52 +0900

    elasticsearch 실습

---
+++ boot-book/src/main/java/com/test/book/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.test.book.repository;
+
+import com.test.book.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+
+}
